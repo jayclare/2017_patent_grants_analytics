90f38e50d0958c667ba99406ad69187948d93042
James Clare <jamesclare@Jamess-MBP.home>
2018-01-12 13:15:11 -0500

    fixed issue with xml cleaning script

--- scripts/xml_cleaner.py
+++ scripts/xml_cleaner.py
@@ -11,8 +11,8 @@ def patent_xml_cleaner(file_list):
     xmltag_pattern = re.compile('^<\?xml.*>$')
 
     for xml_file in file_names:
-        raw_file_path = './raw_data/' + xml_file
-        clean_file_path = './clean_data/clean_' + xml_file
+        raw_file_path = '../raw_data/' + xml_file
+        clean_file_path = '../clean_data/clean_' + xml_file
         with open(raw_file_path, 'r') as f1:
             with open(clean_file_path, 'w') as f2:
                 f2.write('<?xml version="1.0"?>' + '\n')
